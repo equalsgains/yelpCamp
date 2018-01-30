24db33efa098785da363e141db42ab94a2297b89
Luis Quintanilla <equalsgains@gmail.com>
2018-01-29 23:14:09 -0700

    removed array of campgrounds

--- app.js
+++ app.js
@@ -35,44 +35,7 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //   }
 // );
 
-// var campgrounds = [
-//   {
-//     name: "Salmon Creek",
-//     img: "https://farm6.staticflickr.com/5187/5623797406_ea91016ac3.jpg"
-//   },
-//   {
-//     name: "Granite Hill",
-//     img: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"
-//   },
-//   {
-//     name: "Mountain Goat's Rest",
-//     img: "https://farm3.staticflickr.com/2562/3753652224_7ab88a28df.jpg"
-//   },
-//   {
-//     name: "Salmon Creek",
-//     img: "https://farm6.staticflickr.com/5187/5623797406_ea91016ac3.jpg"
-//   },
-//   {
-//     name: "Granite Hill",
-//     img: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"
-//   },
-//   {
-//     name: "Mountain Goat's Rest",
-//     img: "https://farm3.staticflickr.com/2562/3753652224_7ab88a28df.jpg"
-//   },
-//   {
-//     name: "Salmon Creek",
-//     img: "https://farm6.staticflickr.com/5187/5623797406_ea91016ac3.jpg"
-//   },
-//   {
-//     name: "Granite Hill",
-//     img: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"
-//   },
-//   {
-//     name: "Mountain Goat's Rest",
-//     img: "https://farm3.staticflickr.com/2562/3753652224_7ab88a28df.jpg"
-//   }
-// ];
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
