a01cd8b470cdaea370fcb23fc1b84a9a5041d403
Luis Quintanilla <equalsgains@gmail.com>
2018-04-05 00:10:20 -0600

    cleaned up styling on show page

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ mongoose.connection.once("open", () => {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
   res.render("landing");
