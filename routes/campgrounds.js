1b97a87d8847f94f4547d8e258ec31d64d6e58e6
Luis Quintanilla <equalsgains@gmail.com>
2018-06-05 11:45:31 -0600

    hid the edit and delete buttons unless user owns the campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -80,7 +80,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
 });
 
 // UPDATE CAMPGROUND
-router.put("/:id", function (req, res) {
+router.put("/:id", checkCampgroundOwnership, function (req, res) {
   // find and update the correct campground
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function (
     err,
@@ -96,7 +96,7 @@ router.put("/:id", function (req, res) {
 });
 
 // DESTROY CAMPGROUND
-router.delete("/:id", function (req, res) {
+router.delete("/:id", checkCampgroundOwnership, function (req, res) {
   Campground.findByIdAndRemove(req.params.id, function (err) {
     if (err) {
       res.redirect("/campgrounds");
