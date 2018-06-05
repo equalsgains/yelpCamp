f62de2f50e7d7d759238fcad437a9d303d88ccc7
Luis Quintanilla <equalsgains@gmail.com>
2018-06-04 21:58:00 -0600

    created destroy route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -84,16 +84,30 @@ router.get("/:id/edit", function(req, res) {
 });
 
 // UPDATE CAMPGROUND
-router.put("/:id", function(req, res){
+router.put("/:id", function(req, res) {
   // find and update the correct campground
-    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
-      if (err){
-        res.redirect("/campgrounds");
-      } else {
-        res.redirect("/campgrounds/" + req.params.id);
-      }
-    })
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(
+    err,
+    updatedCampground
+  ) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
   // redirect somewhere show page
+});
+
+// DESTROY CAMPGROUND
+router.delete("/:id", function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 function isLoggedIn(req, res, next) {
